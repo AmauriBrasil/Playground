0a1b9f355d8fd1ad68a9f4208f598c2c3466393d
AmauriBrasil <amauribrasil80@gmail.com>
2024-11-09 13:31:45 -0300

    ajustando o script de teste IFrame

--- cypress/e2e/iframe.cy.js
+++ cypress/e2e/iframe.cy.js
@@ -5,12 +5,14 @@ describe('Iframe', () => {
         cy.goTo('/iframe', 'IFrame')
     })
 
-    it('Deve preencher o nome em uma página que tem Iframe', () => {
+    it.only('Deve preencher o nome em uma página que tem Iframe', () => {
         cy.get('[data-cy="iframe-inputs"]')
-            .its('0.contentDocument.body') // acessa o body do conteúdo do iframe.
-            .should('not.be.empty') // Garantir que o body não está vazio antes de prosseguir
-            .then(cy.wrap) // Embrulha o 'body' para que possa usar comandos do Cypress (find, type, etc.) diretamente nele.
-            .find('#fullname')
-            .type('Fernando Papito')
+            .its('0.contentDocument.body') // É usado para alcançar o body dentro do primeiro (e único) iframe encontrado no seletor
+            .should('not.be.empty') // Verifica se o body não está vazio, garantindo que o conteúdo do iframe foi carregado
+            .then((body) => { // Usa then para obter o body do iframe como um parâmetro (aqui chamado body)
+                cy.wrap(body) //Embrulha" o body do iframe para que possamos usar comandos Cypress, como find e type, diretamente nele.
+                    .find('#fullname') //Procura dentro do body do iframe o elemento com o ID
+                    .type('Fernando Papito') //Insere o texto "Fernando Papito" no campo #fullname dentro do iframe
+            })
     })
 })
